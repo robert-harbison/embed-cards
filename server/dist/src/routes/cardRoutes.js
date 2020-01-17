cb1e0b83580893d9974af37a470a8b810edf5f4b
Robert Harbison <robertharbison@Roberts-iMac.local>
2020-01-17 00:08:45 -0800

    Base base client and server

---
+++ server/dist/src/routes/cardRoutes.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+
+router.get('/repo-card', function (req, res, next) {
+	res.render('index', { title: 'Express' });
+});
+
+router.get('/user-card', function (req, res, next) {
+	res.send('user card');
+});
+
+module.exports = router;
---
+++ server/dist/src/server.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+
+var cardRoutes = require('./routes/cardRoutes');
+
+var app = express();
+
+// View Engine Setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/cards', cardRoutes);
+
+// Catch 404
+app.use(function (req, res, next) {
+	res.status(404).send('Not Found');
+});
+
+module.exports = app;
